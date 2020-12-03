99981b6ab57a81f7bbf975312d22d9a7850106d0
Carles Sentis <carles.sentis@transactcampus.com>
2020-12-03 14:38:08 +0100

    Initial commit

---
+++ src/com/codeskraps/y2020/Main.kt
@@ -0,0 +1,27 @@
+@file:Suppress("ConstantConditionIf")
+
+package com.codeskraps
+
+import com.codeskraps.y2020.days.*
+import java.util.*
+import java.util.function.Consumer
+
+object Main {
+
+    // Settings
+    private const val RUN_ALL = true
+    const val DEBUG = false
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val days = ArrayList<Day>()
+        days.add(Day1())
+        days.add(Day2())
+        days.add(Day3())
+        //days.add(Day4())
+
+        if (RUN_ALL) {
+            days.forEach(Consumer { obj: Day -> obj.run() })
+        } else days[days.size - 1].run()
+    }
+}
---
+++ src/com/codeskraps/y2020/days/Day4.kt
@@ -0,0 +1,15 @@
+package com.codeskraps.y2020.days
+
+class Day4 : Day() {
+
+    override val day: String
+        get() = "Four"
+
+    override fun partOne(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun partTwo(): String {
+        TODO("Not yet implemented")
+    }
+}
